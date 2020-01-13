d54bf7f4ca83765381738da146ea5b905b90f6a5
Peter Wagstaff <peterw1314@gmail.com>
2020-01-13 12:23:30 -0500

    Delete GlobalVariables.java

--- src/GlobalVariables.java
+++
@@ -1,8 +0,0 @@
-public class GlobalVariables {
-
-    public static final int MAX_GENERATIONS = 7;
-    public static int dispayWidth = 800;
-    public static int dispayHeight = 500;
-    public static int nodeSize = 25;
-
-}
